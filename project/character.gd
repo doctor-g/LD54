e0fd6262be35455aff1b9bb36723458bd31ed8f8
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-09-30 09:40:52 -0400

    Control the player and hammer
    They are implemented as character bodies with hand-computed physics
    rather than the physics engine.

--- project/character.gd
+++ project/character.gd
@@ -1,22 +1,50 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-@export var power := 5000
-@export var movement_power := 7000
+
+@export var mass := 70.0
+@export var walking_speed := 100
+
+@export_category("Rope")
+@export var min_rope_length := 75.0
+@export var max_rope_length := 250.0
+@export var rope_adjustment_speed := 70.0
+
+@export_category("Hammer")
+@export var hammer_mass := 10.0
+@export var min_rotation_speed := TAU / 2.0
+@export var max_rotation_speed := TAU * 3.0
+@export var rotation_adjustment_speed := TAU / 8.0
+## An extra factor by which the hammer's rotational speed affects the player's position.
+## Tuned for a good game feel.
+@export var fudge_factor := 2.0
+
+var _angle_to_hammer := 0.0
+var _rotation_speed := PI * 1.5
+var _rope_length := 150.0
 
 @onready var _hammer := $Hammer
 
-var _force_vector : Vector2
 
 func _physics_process(delta):
-	# Player-controlled movement feels better when it is directly
-	# manipulating the position. Otherwise, using forces and physics,
-	# it is realistic but not responsive enough to be fun.
 	var direction := Vector2(
 		Input.get_axis("move_left", "move_right"),
 		Input.get_axis("move_up", "move_down")
 	)
-	position += direction * 100 * delta
+	position += direction * walking_speed * delta
+
+	# Adjust rope length
+	var rope_adjustment := Input.get_axis("reel_in", "let_out")
+	_rope_length = clampf(_rope_length + rope_adjustment * rope_adjustment_speed * delta,
+		min_rope_length, max_rope_length)
+		
+	# Adjust spinning speed
+	var power_adjustment := Input.get_axis("decrease_power", "increase_power")
+	_rotation_speed = clampf(_rotation_speed + power_adjustment * delta * rotation_adjustment_speed,
+		min_rotation_speed, max_rotation_speed)
 	
-	# Swing the hammer
-	_force_vector = Vector2(0,power).rotated(_hammer.rotation)
-	_hammer.apply_force(_force_vector)
+	# Adjust hammer position
+	_angle_to_hammer += _rotation_speed * delta
+	_hammer.position = Vector2(cos(_angle_to_hammer), sin(_angle_to_hammer)) * _rope_length
+	
+	# _rotation_speed here is a proxy for "power" being applied
+	position += Vector2(1,0).rotated(_angle_to_hammer) * hammer_mass / mass * _rotation_speed * fudge_factor
